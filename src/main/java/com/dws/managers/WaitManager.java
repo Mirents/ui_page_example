098da2d810555683b572dc4d0b1521aee18b88a6
Mirents <mirent@mail.ru>
2022-01-17 23:44:24 +0300

    Rename variable INSTANCE

--- src/main/java/com/dws/managers/WaitManager.java
+++ src/main/java/com/dws/managers/WaitManager.java
@@ -7,16 +7,16 @@ import static com.dws.utils.ProperitesConstant.WAIT_TIMEOUTINSECONDS;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitManager {
-    private static WebDriverWait INSTANCE = null;
+    private static WebDriverWait instance = null;
     
     public static WebDriverWait getWaitManager() {
-        if(INSTANCE == null) {
+        if(instance == null) {
             long waitTime = Long.parseLong(getThisProperties()
                 .getProperty(WAIT_TIMEOUTINSECONDS));
             long waitSleep = Long.parseLong(getThisProperties()
                     .getProperty(WAIT_SLEEPINMILLIS));
-            INSTANCE = new WebDriverWait(getDriver(), waitTime, waitSleep);
+            instance = new WebDriverWait(getDriver(), waitTime, waitSleep);
         }
-        return INSTANCE;
+        return instance;
     }
 }
