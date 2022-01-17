b800991c54d2dce0ccfe9cdc77ca25c409c73ffe
Mirents <mirent@mail.ru>
2022-01-17 23:36:37 +0300

    Replace ProductHelper, Rename variable INSTANCE

--- src/main/java/com/dws/helper/CartHelper.java
+++ src/main/java/com/dws/helper/CartHelper.java
@@ -1,5 +1,6 @@
 package com.dws.helper;
 
+import com.dws.entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
@@ -7,33 +8,33 @@ import org.slf4j.LoggerFactory;
 
 public class CartHelper {
     protected static final Logger LOGGER = LoggerFactory.getLogger(CartHelper.class);
-    private static CartHelper INSTANCE;
-    private List<ProductHelper> cart;
+    private static CartHelper instance;
+    private List<Product> cart;
     
     public static CartHelper getCartHelper() {
-        if(INSTANCE == null) {
-            INSTANCE = new CartHelper();
+        if(instance == null) {
+            instance = new CartHelper();
         }
-        return INSTANCE;
+        return instance;
     }
 
     private CartHelper() {
         this.cart = new ArrayList<>();
     }
     
-    public void addProduct(ProductHelper product) {
+    public void addProduct(Product product) {
         cart.add(product);
     }
     
     public boolean isExistsProductByName(String name) {
-        for(ProductHelper ph: cart)
+        for(Product ph: cart)
             if(ph.getName().equals(name))
                 return true;
         return false;
     }
     
-    public ProductHelper getProductByName(String name) {
-        for(ProductHelper ph: cart)
+    public Product getProductByName(String name) {
+        for(Product ph: cart)
             if(ph.getName().equals(name))
                 return ph;
         return null;
@@ -41,14 +42,14 @@ public class CartHelper {
     
     public float getTotalPrice() {
         float total = 0.0f;
-        for(ProductHelper ph: cart)
+        for(Product ph: cart)
             total += (ph.getPrice() * ph.getQuantity());
         return total;
     }
     
     public int getTotalQuantity() {
         int total = 0;
-        for(ProductHelper ph: cart)
+        for(Product ph: cart)
             total += ph.getQuantity();
         return total;
     }
@@ -60,7 +61,7 @@ public class CartHelper {
     @Override
     public String toString() {
         String data = "";
-        for(ProductHelper ph: cart)
+        for(Product ph: cart)
             data += ph.getName() + " - " + ph.getPrice() + " - "
                     + ph.getQuantity();
         LOGGER.debug(data);
