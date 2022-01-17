5f988c2f3139b9c1485b59cc523fe7ae57f3f03f
Mirents <mirent@mail.ru>
2022-01-17 23:15:18 +0300

    Add file constant with excepted results

--- src/test/java/com/dws/test/base/ExpectedResultConstant.java
+++ src/test/java/com/dws/test/base/ExpectedResultConstant.java
@@ -1,5 +1,13 @@
 package com.dws.test.base;
 
-public abstract class ExpectedResultConstant {
+public class ExpectedResultConstant {
+    public final static String COLOR_ERROR_MESSAGE = "204, 0, 0";
     
+    public final static String TEXT_ERROR_MESSAGE = 
+            "Quantity should be positive";
+    
+    public final static String COLOR_SUCCESS_MESSAGE = "145, 189, 9";
+    
+    public final static String TEXT_SUCCESS_MESSAGE = 
+            "The product has been added to your shopping cart";
 }
